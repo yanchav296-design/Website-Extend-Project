afe69c381b207140d15446668faa298fc722d520
Yang <yanchav@dunwoody.edu>
2025-10-02 09:44:59 -0500

    Chaotic update

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
-import flask from Flask
+from flask import Flask, render_template
+
+app = Flask(_name_)
+
+@app.route('/')
+def index():
+    return redirect(url_for('carson'))
+
+@app.route('/carson', methods=[])
+
+def carson():
+    return
+
+
 
