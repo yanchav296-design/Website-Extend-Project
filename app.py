ca146f789f61e1ede6f889569e50c683990fea64
yanchav296 <yanchav296@gmail.com>
2025-10-09 10:47:52 -0500

    Eric change

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
  
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newgame.db'
